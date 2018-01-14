17a162573884abd5920b2f5e6658a62627355177
AmusementAdvisor <35181743+AmusementAdvisor@users.noreply.github.com>
2018-01-14 21:36:27 +0100

    Update tabela.js

--- tabela.js
+++ tabela.js
@@ -1,4 +1,69 @@
 
+var podaci = [
+  {
+    "Ime":"Cheers",
+    "Ocjena":"3",
+    "Rang_cijena":"5",
+    "Kapacitet":"150",
+    "Datum_sljedeceg_eventa":"25.12.2017.",
+  },
+  {
+    "Ime":"Pasta ZEN",
+    "Ocjena":"4",
+    "Rang_cijena":"8",
+    "Kapacitet":"65",
+    "Datum_sljedeceg_eventa":"15.12.2017.",
+  },
+   {
+    "Ime":"Brew pub",
+    "Ocjena":"5",
+    "Rang_cijena":"7",
+    "Kapacitet":"70",
+    "Datum_sljedeceg_eventa":"15.12.2017.",
+  },
+  {
+    "Ime":"Mikser",
+    "Ocjena":"5",
+    "Rang_cijena":"6",
+    "Kapacitet":"500",
+    "Datum_sljedeceg_eventa":"20.12.2017.",
+  },
+  {
+    "Ime":"Revolucija 1764",
+    "Ocjena":"4",
+    "Rang_cijena":"8",
+    "Kapacitet":"40",
+    "Datum_sljedeceg_eventa":"31.12.2017.",
+  },
+  {
+    "Ime":"Ort",
+    "Ocjena":"4",
+    "Rang_cijena":"4",
+    "Kapacitet":"45",
+    "Datum_sljedeceg_eventa":"24.12.2017.",
+  },
+  {
+    "Ime":"Cinemas Sloga",
+    "Ocjena":"2",
+    "Rang_cijena":"4",
+    "Kapacitet":"600",
+    "Datum_sljedeceg_eventa":"24.12.2017.",
+  },
+  {
+    "Ime":"Vatra",
+    "Ocjena":"4",
+    "Rang_cijena":"8",
+    "Kapacitet":"80",
+    "Datum_sljedeceg_eventa":"24.12.2017.",
+  },
+  {
+    "Ime":"Woki",
+    "Ocjena":"3",
+    "Rang_cijena":"3",
+    "Kapacitet":"100",
+    "Datum_sljedeceg_eventa":"26.12.2017.",
+  }
+];
 
 $(document).ready(function () {
         var tr;
@@ -17,7 +82,7 @@ $(document).ready(function () {
             tr.cells[0].appendChild(checkbox);
             tr.cells[1].appendChild(document.createTextNode(podaci[i].Ime));
             tr.cells[2].appendChild(document.createTextNode(podaci[i].Ocjena));
-            tr.cells[3].appendChild(document.createTextNode(podaci[i].Rang_cijena(1-10)));
+            tr.cells[3].appendChild(document.createTextNode(podaci[i].Rang_cijena));
             tr.cells[4].appendChild(document.createTextNode(podaci[i].Kapacitet));
             tr.cells[5].appendChild(document.createTextNode(podaci[i].Datum_sljedeceg_eventa));
           
@@ -25,8 +90,6 @@ $(document).ready(function () {
         }
         
 });
-
-
 
 function sortirajRastuci(kolona){
     var tabela = document.getElementById('tabela');
@@ -67,9 +130,9 @@ function sortirajOpadajuci(kolona){
             }
             else {
                 if (celija1.innerHTML > celija2.innerHTML){
-                    var temp = tabela.rows[i].innerHTML;
+                    var temp2 = tabela.rows[i].innerHTML;
                     tabela.rows[i].innerHTML = tabela.rows[j].innerHTML;
-                    tabela.rows[j].innerHTML = temp;
+                    tabela.rows[j].innerHTML = temp2;
                 }
             }
         }
@@ -77,9 +140,9 @@ function sortirajOpadajuci(kolona){
 }
 
 function oznaciSve(){
-    $(".cb").attr("checked",$("#oznSve").is(":checked"));   
+    $(".cb").attr("checked",$("#oznSve").is(":checked"));
 }
-
+  
 
 function obrisi(){
     $('#tabela tbody').find('tr').each(function () {
@@ -88,4 +151,37 @@ function obrisi(){
              $(this).closest("tr").remove();  
         }
     }); 
+}
+
+function dodaj(){
+  var i = 9;
+	var a = document.getElementById('ime').value;
+	var b = document.getElementById('ocjena').value;
+	var c = document.getElementById('rang').value;
+	var d = document.getElementById('kapacitet').value;
+	var e = document.getElementById('datum').value;
+	if(a !== "" && b !== "" && c !== "" && d !== "" && e !== "") {
+	tr = document.createElement('tr');
+	for (var j = 0 ; j < 6; j++)
+        tr.appendChild(document.createElement('td'));
+  var checkbox = document.createElement('input');
+  checkbox.type = "checkbox";
+  checkbox.className = "cb";
+  var pom = i+1;
+  checkbox.id = "cb"+pom;
+  tr.cells[0].appendChild(checkbox);
+  tr.cells[1].appendChild(document.createTextNode(a));
+  tr.cells[2].appendChild(document.createTextNode(b));
+  tr.cells[3].appendChild(document.createTextNode(c));
+  tr.cells[4].appendChild(document.createTextNode(d));
+  tr.cells[5].appendChild(document.createTextNode(e));
+  $('#tabela').append(tr);
+	}
+	else {
+		document.getElementById('ime').style.border = "1px solid red";
+		document.getElementById('ocjena').style.border = "1px solid red";
+		document.getElementById('rang').style.border = "1px solid red";
+		document.getElementById('kapacitet').style.border = "1px solid red";
+		document.getElementById('datum').style.border = "1px solid red";
+	}
 }
